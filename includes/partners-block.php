0d7d24a067f01df67a8556d604a753e39a77ab07
John Yoder <johngilesyoder@gmail.com>
2015-03-10 15:07:47 -0600

    Experience page marked up and styled. (not responsive)

---
+++ includes/partners-block.php
@@ -0,0 +1,42 @@
+<section class="section-block section-partners">
+  <div class="section-header">
+    <h2 class="section-title">Partners</h2>
+    <a href="#">View All HATCH Partners<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-link-chevron.svg"></a>
+    <a href="#">Become a Sponsor<img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-link-chevron.svg"></a>
+  </div>
+  <div class="row">
+    <a href="#" class="innovation-partner">
+      <div class="innovation-partner-wrapper">
+        <span>Innovation Partner</span>
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partner-intel.png">
+      </div>
+    </a>
+    <div class="partners-column partners-platinum">
+      <h3 class="partners-column-title">Platinum</h3>
+      <div class="partner-logos">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+      </div>
+    </div>
+    <div class="partners-column partners-gold">
+      <h3 class="partners-column-title">Gold</h3>
+      <div class="partner-logos">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+      </div>
+    </div>
+    <div class="partners-column partners-silver">
+      <h3 class="partners-column-title">Silver</h3>
+      <div class="partner-logos">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+        <img class="partner-logo" src="http://placehold.it/95x95">
+      </div>
+    </div>
+  </div>
+</section>
--- template-experience.php
+++ template-experience.php
@@ -1,5 +1,166 @@
 <?php /* Template Name: Experience Template */ get_header(); ?>
   
-  CONTENT
+  <section class="section-block section-hero">
+  	<div class="hero-content-wrapper">
+  		<div class="conference-apply">
+	  		<div class="hero-title">
+		  		<h1 class="hatch-logo hero-logo">HATCH<sup>12</sup></h1>
+		  		<span class="hero-date-location">Sep 16-20, 2015 &nbsp;//&nbsp; Bozeman, Montana</span>
+		  	</div>
+		  	<div>
+	  			<a href="#" class="btn btn-apply">Invite Only – <strong>Apply Now</strong></a>
+	  		</div>
+	  	</div>
+  	</div>
+  </section>
+
+  <section class="section-block section-what">
+  	<div class="container">
+  		<h2 class="section-title">What exactly is <span class="hatch-logo">HATCH</span>?</h2>
+  		<div class="what-video">
+  			<div class="what-video-wrapper">
+  				<iframe width="730" height="411" src="https://www.youtube.com/embed/MV4rwFw94aw?rel=0" frameborder="0" allowfullscreen></iframe>
+  			</div>
+  		</div>
+  		<div class="what-summary">
+  			<p>HATCH is a community, movement, and a series of experiences designed to ACTIVATE CREATIVITY to HATCH a better world. In it's 12th year, HATCH experience is a four day annual retreat meets summit gifted to 100 of the most provocative innovators, inventors, and cultural catalysts, across a wide range of disciplines.</p>
+  			<p>Attendees who are invited each year cover a wide swath of humanity - Oscar-winning filmmakers, Grammy-winning Musicians, CEO’s and Founders of diverse companies and non- profits, designers from IDEO, NASA, Google, and a wide range of hackers, inventors, educators, disruptors, and mavericks.</p>
+  			<p>This cross-pollinated alchemy of global perspectives, allows thought leaders to re-ignite their creative vision... and opens them up to a new a network for life. The outcome has lead to hundreds new projects and collaborations, and ongoing mentorship opportunities. &nbsp;<a href="#">Learn more</a></p>
+  		</div>
+  		<aside class="section-aside hard-to-explain">
+  			<div class="row">
+  				<div class="section-aside-title">
+	  				<h3>It's hard to explain</h3>
+	  			</div>
+	  			<div class="section-aside-content">
+	  				<div class="section-aside-content-wrapper">
+			  			<div class="quote-block">
+			  				<div class="quote-statement">
+			  					<p>“HATCH is something you can’t really put into words because there doesn’t seem to be an adequate way to verbally describe something that is so genuinely life changing for everyone there — It truly is. The people that are curated are brilliant, smart, talented, and giving. HATCH is beyond a doubt the best thing I have ever experienced. It’s changed how I do business — and how I live life.”</p>
+			  				</div>
+			  				<cite>&mdash;&nbsp; <span class="cite-person">Grace Davis</span> <span class="cite-organization">INTEL</span></cite>
+			  			</div>
+			  		</div>
+		  		</div>
+  		</aside>
+  	</div>
+  </section>
+
+  <section class="section-block section-why">
+  	<div class="container">
+  		<h2 class="section-title">Why does <span class="hatch-logo">HATCH</span> exist?</h2>
+  		<div class="why-blocks">
+  			<div class="why-block">
+  				<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/experience/why-block-1.jpg">
+  				<h3>Reversing the Creativity Crisis</h3>
+  				<p>Creativity is the magic ingredient in building a bright future.  HATCH is driven to infuse every member of the HATCH community by: uniting disruptors and mavericks, cross-pollinating diverse people and ideas, and creating experiences that activate the creator inside each and every person. &nbsp;<a href="#">Learn more about the Creativity Crisis</a></p>
+  			</div>
+  			<div class="why-block">
+  				<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/img/experience/why-block-2.jpg">
+  				<h3>360&deg; Mentorship</h3>
+  				<p>Mentoring is a Hatch cornerstone. HATCH is built around meaningful relationships. What began as top-down mentorship has evolved into a ‘360’ mentorship approach; bottom-up and peer to peer. We encourage — if not expect — all HATCH community members to mentor, share, and challenge one another. &nbsp;<a href="#">Learn more about 360° Mentorships</a></p>
+  			</div>
+  		</div>
+  		<div class="because-of-hatch">
+  			<h2 class="because-of-hatch-title">#BecauseOfHATCH</h2>
+  			<div id="carousel-because-of-hatch" class="carousel carousel-fade slide" data-ride="carousel">
+  			  <!-- Wrapper for slides -->
+  			  <div class="carousel-inner" role="listbox">
+  			    <div class="item active">
+							<p>Entrepreneur returns to her community, founds an entrepreneurial mentorship incubator.</p>
+  			    </div>
+  			    <div class="item">
+  			      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio.</p>
+  			    </div>
+  			    <div class="item">
+  			      <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem.</p>
+  			    </div>
+  			  </div>
+  			</div>
+  		</div>
+  	</div>
+  </section>
+
+  <section class="section-block section-who">
+  	<div class="container">
+  		<h2 class="section-title">You will be <span class="hatch-logo">changed</span>.</h2>
+  		<p class="who-summary">Each year, the HATCH group is diverse and includes artists, inventors, award-winning authors, grammy-winning musicians, filmmakers, technologists, CEOs, social impact change makers, designers, and more.</p>
+  		<div class="testimonial">
+  			<img class="testimonial-photo" src="<?php echo get_template_directory_uri(); ?>/assets/img/experience/testimonial-rahi.png">
+  			<div class="quote-block">
+  				<div class="quote-statement">
+  					<p>“Imagine gathering up a hundred innovators, artists, makers, creators, educators, risk-takers, influencers, and mavericks... people who want to shake the world by its ankles. And are destined to.</p>
+						<p>Imagine them in a room together, teased out of their cocoons, grids off, brains on, glass flying everywhere: A NASA engineer tackles a problem for an entrepreneur. A scientist cracks open an idea for music distribution. An ad executive delivers insight to a health-care play. An inventor, architect, and designer sketching new models for education... Imagine that. Or just find yourself at HATCH.”</p>
+  				</div>
+  				<cite>&mdash;&nbsp; <span class="cite-person">San Rahi</span> <span class="cite-organization">CEO MANTRII / EMMY-WINNING CREATIVE DIRECTOR</span></cite>
+  			</div>
+  		</div>
+  		<div class="testimonial">
+  			<img class="testimonial-photo" src="<?php echo get_template_directory_uri(); ?>/assets/img/experience/testimonial-anne.png">
+  			<div class="quote-block">
+  				<div class="quote-statement">
+  					<p>“I have been to the world’s top conferences and retreats, but I have never been surrounded, by people - and an environment so powerful as HATCH. I am re-HATCHed”</p>
+					</div>
+  				<cite>&mdash;&nbsp; <span class="cite-person">Anne Mahlum</span> <span class="cite-organization">FOUNDER, BACK ON MY FEET</span></cite>
+  			</div>
+  		</div>
+  		<div class="testimonial">
+  			<img class="testimonial-photo" src="<?php echo get_template_directory_uri(); ?>/assets/img/experience/testimonial-travis.png">
+  			<div class="quote-block">
+  				<div class="quote-statement">
+  					<p>”This meeting of visionaries is the most real life example of the adage "come as a stranger, leave as a friend" that I have been involved in - a family is formed, and through the influence of others, one’s path becomes more meaningful.”</p>
+					</div>
+  				<cite>&mdash;&nbsp; <span class="cite-person">Will Travis</span> <span class="cite-organization">PRESIDENT, SID LEE</span></cite>
+  			</div>
+  		</div>
+  		<div class="testimonial">
+  			<img class="testimonial-photo" src="<?php echo get_template_directory_uri(); ?>/assets/img/experience/testimonial-hoffer.png">
+  			<div class="quote-block">
+  				<div class="quote-statement">
+  					<p>“What became instantly clear at HATCH is that the state of my health, our community’s health, and the world's health, are all directly proportional to the degree to which creativity is truly empowered in the world. Creativity is the catalyst that will solve worlds toughest problems. It’s the engine that drives advancement in the sciences. It gives our life meaning. Its a common thread in our humanity, and it seeds the answers to tomorrows prayers. And thats why anything that can empower that faculty in us in such a powerful way is a game changer. And thats why I'm thankful I was invited to - and impacted by Hatch. But I also plan to remember, Hatch is not a noun. Hatch is a verb.”</p>
+  				</div>
+  				<cite>&mdash;&nbsp; <span class="cite-person">Deej Hoffer</span> <span class="cite-organization">MUSIC PRODUCER, COMPOSER</span></cite>
+  			</div>
+  		</div>
+  	</div>
+  </section>
+
+  <section class="section-block section-where">
+  	<div class="container">
+  		<h2 class="section-title">In the mountains of <span class="hatch-logo">Montana</span>.</h2>
+  		<div class="where-summary">
+  			<p><strong>BOZEMAN, MT</strong> — Consistently named one of the best towns in America by publications such as Outdoor Magazine, Business Week, USA Today, New York Times, Forbes, and more. Bozeman is an epicenter of creativity and innovation, and home to Montana State University. The area is a destination for some of the world’s most coveted fly-fishing, and was the central location for “A River Runs Through It.” Southwest Montana is a cultural and scenic oasis, and is only an hour outside of the stunning Yellowstone Park. Within the last decade, Montana has also become a place where innovators like Bill Gates, Justin Timberlake, Peter Chernin, and Larry Ellison converge to re-calibrate on how they are impacting the world.</p>
+  		</div>
+  		<aside class="section-aside off-the-grid">
+  			<div class="row">
+  				<div class="section-aside-title">
+	  				<h3>Off the grid</h3>
+	  			</div>
+	  			<div class="section-aside-content">
+	  				<div class="section-aside-content-wrapper">
+			  			<p>Part of the HATCH experience is a two ￼day “OFF THE GRID” retreat / summit. The fast lane disappears and we un-plug to reconnect. No cell service. No wi-fi. Only nature and round-table Think Tanks with extraordinary peers while immersed in the refueling surroundings of Montana.</p>
+			  		</div>
+		  		</div>
+  		</aside>
+  		<img class="bubble bubble-1" src="<?php echo get_template_directory_uri(); ?>/assets/img/experience/where-bubble-1.jpg">
+  		<img class="bubble bubble-2" src="<?php echo get_template_directory_uri(); ?>/assets/img/experience/where-bubble-2.jpg">
+  	</div>
+  </section>
+
+  <section class="section-block section-bottom">
+  	<div class="container">
+  		<div class="conference-apply">
+	  		<div class="hero-title">
+		  		<h1 class="hatch-logo hero-logo">HATCH<sup>12</sup></h1>
+		  		<span class="hero-date-location">Sep 16-20, 2015 &nbsp;//&nbsp; Bozeman, Montana</span>
+		  	</div>
+		  	<div>
+	  			<a href="#" class="btn btn-apply">Invite Only – <strong>Apply Now</strong></a>
+	  		</div>
+	  	</div>
+  		<!-- Partners -->
+    	<?php get_template_part( 'includes/partners-block' ); ?>
+  	</div>
+  </section>
 
 <?php get_footer(); ?>
